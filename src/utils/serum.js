99375f41963fa91d086f8ec051bb8f6552ede855
Tommy Johnson <tommyjohnson1028@gmail.com>
2021-02-20 19:32:09 -0800

    add getBidAskSpread

--- src/utils/serum.js
+++ src/utils/serum.js
@@ -1,14 +1,30 @@
 import { Market, MARKETS } from '@project-serum/serum';
 
-/**
- * 
- * @param {Connection} connection 
- * @param {PublicKey} marketAddress 
- */
-export const getSerumMarket = async (
-  connection,
-  marketAddress,
-) => {
-  const programId = MARKETS.find(({ deprecated }) => !deprecated).programId;
-  return Market.load(connection, marketAddress, {}, programId);
+
+export class CLOBDex {
+
+  constructor(connection, marketAddress) {
+    this.connection = connection;
+    this.marketAddress = marketAddress;
+    this.market = CLOBDex.getSerumMarket();
+  }
+
+  /**
+   * 
+   * @param {Connection} connection 
+   * @param {PublicKey} marketAddress 
+   */
+  static getSerumMarket = async () => {
+    const programId = MARKETS.find(({ deprecated }) => !deprecated).programId;
+    return Market.load(this.connection, this.marketAddress, {}, programId);
+  }
+
+  getBidAskSpread = async () => {
+    const bidOrderbook = await this.market.loadBids(connection);
+    const askOrderbook = await this.market.loadAsks(connection);
+
+    const highestbid = bidOrderbook.getL2(1)[0];
+    const lowestAsk = askOrderbook.getL2(1)[0];
+    return {bid: highestbid[1], ask: lowestAsk[1]}
+  }
 }

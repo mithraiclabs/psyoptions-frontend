5e3c2fdee23f5b7b866a1d1c548dc1ffd271560a
Tommy Johnson <tommyjohnson1028@gmail.com>
2021-02-21 13:14:01 -0800

    add getPrice

--- src/utils/serum.js
+++ src/utils/serum.js
@@ -1,12 +1,12 @@
 import { Market, MARKETS } from '@project-serum/serum';
 
 
-export class CLOBDex {
+export class SerumMarket {
 
   constructor(connection, marketAddress) {
     this.connection = connection;
     this.marketAddress = marketAddress;
-    this.market = CLOBDex.getSerumMarket();
+    this.market = await SerumMarket.getMarket();
   }
 
   /**
@@ -14,17 +14,28 @@ export class CLOBDex {
    * @param {Connection} connection 
    * @param {PublicKey} marketAddress 
    */
-  static getSerumMarket = async () => {
+  static getMarket = async () => {
     const programId = MARKETS.find(({ deprecated }) => !deprecated).programId;
     return Market.load(this.connection, this.marketAddress, {}, programId);
   }
 
+  /**
+   * Returns the highest bid price and lowest ask price for a market
+   */
   getBidAskSpread = async () => {
+    if (!this.market) {
+      return {bid: null, ask: null}
+    }
     const bidOrderbook = await this.market.loadBids(connection);
     const askOrderbook = await this.market.loadAsks(connection);
 
     const highestbid = bidOrderbook.getL2(1)[0];
     const lowestAsk = askOrderbook.getL2(1)[0];
-    return {bid: highestbid[1], ask: lowestAsk[1]}
+    return {bid: highestbid[1], ask: lowestAsk[1]};
+  }
+
+  getPrice = async () => {
+    const {bid, ask} = await this.getBidAskSpread();
+    return ((ask - bid) / 2) + bid
   }
 }

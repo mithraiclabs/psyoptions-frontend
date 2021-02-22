598990beb0123a496e890009ea7da1bf72ff491d
Taylor123 <cjohns28@terpmail.umd.edu>
2021-02-21 19:27:21 -0800

    remove context, just use memoized hook

--- src/context/store.js
+++ src/context/store.js
@@ -2,7 +2,6 @@ import React, { cloneElement } from 'react'
 import { ThemeProvider } from '@material-ui/core/styles'
 
 import { ConnectionProvider } from './ConnectionContext'
-import { OpenPositionsProvider } from './OpenPositionsContext'
 import { OwnedTokenAccountsProvider } from './OwnedTokenAccounts'
 import { WalletProvider } from './WalletContext'
 import theme from '../utils/theme'
@@ -12,7 +11,6 @@ const providers = [
   <ConnectionProvider />,
   <WalletProvider />,
   <OwnedTokenAccountsProvider />,
-  <OpenPositionsProvider />,
 ]
 
 // flatten context providers for simpler app component tree
--- src/hooks/useOpenPositions.js
+++ src/hooks/useOpenPositions.js
@@ -1,6 +1,24 @@
-import { useContext } from 'react'
-import { OpenPositionsContext } from '../context/OpenPositionsContext'
+import { useMemo } from 'react'
+import useOptionsMarkets from './useOptionsMarkets'
+import useOwnedTokenAccounts from './useOwnedTokenAccounts'
 
-const useOpenPositions = () => useContext(OpenPositionsContext)
+const useOpenPositions = () => {
+  const { markets } = useOptionsMarkets()
+  const ownedTokens = useOwnedTokenAccounts()
+
+  return useMemo(
+    () =>
+      Object.values(markets).reduce((acc, market) => {
+        const accountsWithHoldings = ownedTokens[
+          market.optionMintAddress
+        ]?.filter((optionTokenAcct) => optionTokenAcct.amount > 0)
+        if (accountsWithHoldings?.length) {
+          acc[market.key] = accountsWithHoldings
+        }
+        return acc
+      }, {}),
+    [markets, ownedTokens]
+  )
+}
 
 export default useOpenPositions

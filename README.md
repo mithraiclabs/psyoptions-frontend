54142bd8fc1b7174e987e4bc9a145fe262ac77ea
Tommy Johnson <tommyjohnson1028@gmail.com>
2021-02-26 20:25:08 -0800

    get program deploying

--- README.md
+++ README.md
@@ -6,3 +6,10 @@ You can also seed without the keypair file by running `npm run seed`.
 Using the keypair file will allow you to mint the SPL tokens the seed generates because the keypair will be the authority of those mints. You can then create the necessary token accounts with the [solana-token cli](https://spl.solana.com/token).
 
 To generate accounts and mint some of the newly generated SPL Tokens run `npm run seed:mintTokens -- PATH_TO_KEYPAIR_FILE WALLET_ADDRESS`. Specify `WALLET_ADDRESS` which will be the owner of the new accounts.
+
+## Deploying Serum
+Set your `solana` configs appropriately. Then run the deployer with `node scripts/deploySerum.js`. 
+
+| Option Flags | Description |
+| ------------ | ----------- |
+| --airdrop | An integer value that determines how much SOL to airdrop to the keypair before running the deployer |
--- scripts/deploySerum.js
+++ scripts/deploySerum.js
@@ -2,6 +2,9 @@ const fs = require('fs');
 const yaml = require('js-yaml');
 const os = require('os');
 const SolWeb3 = require('@solana/web3.js')
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+const argv = require('minimist')(process.argv.slice(2));
 
 const {
   Account,
@@ -40,13 +43,34 @@ const requestAndWaitForAirdrop = async (connection, amount, account) => {
   }
 }
 
-// Get the default keypair and airdrop some tokens
 ;(async () => {
+  // Get the default keypair and airdrop some tokens
   const keyBuffer = fs.readFileSync(solanaConfig.keypair_path);
   const payer = new Account(JSON.parse(keyBuffer));
 
   const connection = new Connection('http://127.0.0.1:8899')
-  await requestAndWaitForAirdrop(connection, 100 * LAMPORTS_PER_SOL, payer);
+  if (argv.airdrop) {
+    const amount = parseInt(argv.airdrop)
+    await requestAndWaitForAirdrop(connection, amount * LAMPORTS_PER_SOL, payer);
+  }
 
-  console.log('*** solana config', solanaConfig);
+  // check that a program key file exists
+  const serumDexKeypairPath = './serum/dex/serum_dex-keypair.json';
+  const localDexProgramKeyFileExists = fs.existsSync(serumDexKeypairPath);
+  console.log('localDexProgramKeyFileExists', localDexProgramKeyFileExists);
+  if (!localDexProgramKeyFileExists) {
+    console.log('\nLocalnet keypair file for the Serum dex program does not exists...creating one\n');
+    const { stdout, stderr } = await exec(`solana-keygen new --no-passphrase --outfile ${serumDexKeypairPath}`);
+    console.log('stdout:', stdout);
+    console.log('stderr:', stderr);
+  }
+
+  // Use the upgradable deployer to deploy the program.
+  // TODO use output stream so there is more user feedback that the program is deploying.
+  console.log('\nDeploying the Serum dex...\n');
+  const { stdout, stderr } = await exec(`solana program deploy --program-id ${serumDexKeypairPath} ./serum/dex/serum_dex.so`)
+  console.log('stdout:', stdout);
+  console.log('stderr:', stderr);
+
+
 })();

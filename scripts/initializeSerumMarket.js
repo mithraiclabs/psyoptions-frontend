ebe93996b3d3252803563671a870865a4902ff65
Taylor123 <cjohns28@terpmail.umd.edu>
2021-02-27 17:44:51 -0800

    vars instead of literals

--- scripts/initializeSerumMarket.js
+++ scripts/initializeSerumMarket.js
@@ -1,10 +1,10 @@
-const fs = require('fs');
-const ScriptHelpers = require('./helpers');
-const SolWeb3 = require('@solana/web3.js');
-const Serum = require('@project-serum/serum');
-const BN = require('bn.js');
-const SerumTokens = require('@project-serum/tokens');
-const SplToken = require('@solana/spl-token');
+const fs = require('fs')
+const ScriptHelpers = require('./helpers')
+const SolWeb3 = require('@solana/web3.js')
+const Serum = require('@project-serum/serum')
+const BN = require('bn.js')
+const SerumTokens = require('@project-serum/tokens')
+const SplToken = require('@solana/spl-token')
 
 const NetworkInfo = require('../src/utils/networkInfo');
 
@@ -19,44 +19,51 @@ const {
   sendAndConfirmTransaction,
   SystemProgram,
   Transaction,
-} = SolWeb3;
-const {
-  DexInstructions,
-  Market,
-  MARKETS,
-  TokenInstructions 
-} = Serum;
+} = SolWeb3
+const { DexInstructions, Market, MARKETS, TokenInstructions } = Serum
 
 const programIds = (networkName) => {
   switch (networkName) {
     case 'Mainnet':
       return {
-        dexProgramPubkey: MARKETS.find(({ deprecated }) => !deprecated).programId,
-        tokenProgramPubkey: new PublicKey('TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA')
+        dexProgramPubkey: MARKETS.find(({ deprecated }) => !deprecated)
+          .programId,
+        tokenProgramPubkey: new PublicKey(
+          'TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA'
+        ),
       }
     case 'Devnet':
       return {
-        dexProgramPubkey: new PublicKey('9MVDeYQnJmN2Dt7H44Z8cob4bET2ysdNu2uFJcatDJno'),
-        tokenProgramPubkey: new PublicKey('TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA')
+        dexProgramPubkey: new PublicKey(
+          '9MVDeYQnJmN2Dt7H44Z8cob4bET2ysdNu2uFJcatDJno'
+        ),
+        tokenProgramPubkey: new PublicKey(
+          'TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA'
+        ),
       }
     case 'Testnet':
       return {
         // Looks like there was no address on the [Serum repo](https://github.com/project-serum/serum-dex#program-deployments)
         dexProgramPubkey: undefined,
-        tokenProgramPubkey: new PublicKey('TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA')
+        tokenProgramPubkey: new PublicKey(
+          'TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA'
+        ),
       }
-    case 'localhost': 
-    const serumDexKeyBuffer = fs.readFileSync(ScriptHelpers.serumDexProgramKeypair);
-    const dexProgramAccount = new Account(JSON.parse(serumDexKeyBuffer));
-    const dexProgramId = dexProgramAccount.publicKey;
-     return {
-       dexProgramPubkey: dexProgramId,
-      tokenProgramPubkey: new PublicKey('TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA')
-     }
+    case 'localhost':
+      const serumDexKeyBuffer = fs.readFileSync(
+        ScriptHelpers.serumDexProgramKeypair
+      )
+      const dexProgramAccount = new Account(JSON.parse(serumDexKeyBuffer))
+      const dexProgramId = dexProgramAccount.publicKey
+      return {
+        dexProgramPubkey: dexProgramId,
+        tokenProgramPubkey: new PublicKey(
+          'TokenkegQfeZyiNwAJbNbGKPFXCWuBvf9Ss623VQ5DA'
+        ),
+      }
     default:
       throw new Error('Unknown network name')
   }
-
 }
 
 /**
@@ -69,84 +76,92 @@ const initializeSerumMarket = async (
   quoteMint,
   baseLotSize,
   quoteLotSize,
-  dexProgramId,
+  dexProgramId
 ) => {
-  const market = new Account();
-  const requestQueue = new Account();
-  const eventQueue = new Account();
-  const bids = new Account();
-  const asks = new Account();
-  const baseVault = new Account();
-  const quoteVault = new Account();
-  const feeRateBps = 0;
-  const quoteDustThreshold = new BN(100);
+  const market = new Account()
+  const requestQueue = new Account()
+  const eventQueue = new Account()
+  const bids = new Account()
+  const asks = new Account()
+  const baseVault = new Account()
+  const quoteVault = new Account()
+  const feeRateBps = 0
+  const quoteDustThreshold = new BN(100)
 
   async function getVaultOwnerAndNonce() {
-    const nonce = new BN(0);
+    const nonce = new BN(0)
     // eslint-disable-next-line no-constant-condition
     while (true) {
       try {
         // eslint-disable-next-line no-await-in-loop
         const vaultOwner = await PublicKey.createProgramAddress(
           [market.publicKey.toBuffer(), nonce.toArrayLike(Buffer, 'le', 8)],
-          dexProgramId,
-        );
-        return [vaultOwner, nonce];
+          dexProgramId
+        )
+        return [vaultOwner, nonce]
       } catch (e) {
-        nonce.iaddn(1);
+        nonce.iaddn(1)
       }
     }
   }
-  const [vaultOwner, vaultSignerNonce] = await getVaultOwnerAndNonce();
+  const [vaultOwner, vaultSignerNonce] = await getVaultOwnerAndNonce()
 
-  console.log('*** TX 1 params',{
-    account: baseVault.publicKey.toString(),
-    mint: baseMint.toString(),
-    owner: vaultOwner.toString(),
-  }, {
-    account: quoteVault.publicKey.toString(),
-    mint: quoteMint.toString(),
-    owner: vaultOwner.toString(),
-  });
+  console.log(
+    '*** TX 1 params',
+    {
+      account: baseVault.publicKey.toString(),
+      mint: baseMint.toString(),
+      owner: vaultOwner.toString(),
+    },
+    {
+      account: quoteVault.publicKey.toString(),
+      mint: quoteMint.toString(),
+      owner: vaultOwner.toString(),
+    }
+  )
 
-  const tx1 = new Transaction();
+  const tx1 = new Transaction()
   // Create an initialize the pool accounts to hold the base and the quote assess
+  const poolSize = 165
+  const poolCostLamports = await connection.getMinimumBalanceForRentExemption(
+    poolSize
+  )
   tx1.add(
     SystemProgram.createAccount({
       fromPubkey: payer.publicKey,
       newAccountPubkey: baseVault.publicKey,
-      lamports: await connection.getMinimumBalanceForRentExemption(165),
-      space: 165,
+      lamports: poolCostLamports,
+      space: poolSize,
       programId: TokenInstructions.TOKEN_PROGRAM_ID,
     }),
     SystemProgram.createAccount({
       fromPubkey: payer.publicKey,
       newAccountPubkey: quoteVault.publicKey,
-      lamports: await connection.getMinimumBalanceForRentExemption(165),
-      space: 165,
+      lamports: poolCostLamports,
+      space: poolSize,
       programId: TokenInstructions.TOKEN_PROGRAM_ID,
     }),
     Token.createInitAccountInstruction(
       TokenInstructions.TOKEN_PROGRAM_ID,
       baseMint,
       baseVault.publicKey,
-      vaultOwner,
+      vaultOwner
     ),
     Token.createInitAccountInstruction(
       TokenInstructions.TOKEN_PROGRAM_ID,
       quoteMint,
       quoteVault.publicKey,
-      vaultOwner,
-    ),
-  );
+      vaultOwner
+    )
+  )
 
-  const tx2 = new Transaction();
+  const tx2 = new Transaction()
   tx2.add(
     SystemProgram.createAccount({
       fromPubkey: payer.publicKey,
       newAccountPubkey: market.publicKey,
       lamports: await connection.getMinimumBalanceForRentExemption(
-        Market.getLayout(dexProgramId).span,
+        Market.getLayout(dexProgramId).span
       ),
       space: Market.getLayout(dexProgramId).span,
       programId: dexProgramId,
@@ -195,8 +210,8 @@ const initializeSerumMarket = async (
       vaultSignerNonce,
       quoteDustThreshold,
       programId: dexProgramId,
-    }),
-  );
+    })
+  )
 
   return {
     tx1,
@@ -204,16 +219,14 @@ const initializeSerumMarket = async (
     tx2,
     signers2: [market, requestQueue, eventQueue, bids, asks],
     market,
-  };
-};
-
-
+  }
+}
 
 ;(async () => {
-  const solanaConfig = ScriptHelpers.getSolanaConfig();
+  const solanaConfig = ScriptHelpers.getSolanaConfig()
   // Get the default keypair and airdrop some tokens
-  const keyBuffer = fs.readFileSync(solanaConfig.keypair_path);
-  const payer = new Account(JSON.parse(keyBuffer));
+  const keyBuffer = fs.readFileSync(solanaConfig.keypair_path)
+  const payer = new Account(JSON.parse(keyBuffer))
 
   const connection = new Connection('https://devnet.solana.com')
 
@@ -221,17 +234,22 @@ const initializeSerumMarket = async (
   // const splData = JSON.parse(
   //   fs.readFileSync('./src/hooks/localnetData.json')
   // )
+
   const splData = getAssetsByNetwork(networks, 'Devnet');
   console.log('*** SPL Data = ', splData);
   process.exit(1);
   // const serumDexKeyBuffer = fs.readFileSync(ScriptHelpers.serumDexProgramKeypair);
   // const dexProgramAccount = new Account(JSON.parse(serumDexKeyBuffer));
   // const dexProgramId = dexProgramAccount.publicKey;
-  const dexProgramId = new PublicKey('9MVDeYQnJmN2Dt7H44Z8cob4bET2ysdNu2uFJcatDJno');
-  console.log(`\nLocated the Serum dex at ${dexProgramId}\n`);
-  console.log(`\nCreating market for\n${splData[0].tokenSymbol} ${splData[0].mintAddress}\n\n${splData[1].tokenSymbol} ${splData[1].mintAddress}\n`);
+  const dexProgramId = new PublicKey(
+    '9MVDeYQnJmN2Dt7H44Z8cob4bET2ysdNu2uFJcatDJno'
+  )
+  console.log(`\nLocated the Serum dex at ${dexProgramId}\n`)
+  console.log(
+    `\nCreating market for\n${splData[0].tokenSymbol} ${splData[0].mintAddress}\n\n${splData[1].tokenSymbol} ${splData[1].mintAddress}\n`
+  )
 
-  const {tx1, signers1, tx2, signers2, market } = await initializeSerumMarket(
+  const { tx1, signers1, tx2, signers2, market } = await initializeSerumMarket(
     connection,
     payer,
     new PublicKey(splData[0].mintAddress),
@@ -239,23 +257,22 @@ const initializeSerumMarket = async (
     // TODO figure out what the hell baseLotSize and quoteLotSize mean
     new BN('100000'),
     new BN('100000'),
-    dexProgramId,
-  );
+    dexProgramId
+  )
 
-  console.log('*** sending TX 1');
+  console.log('*** sending TX 1')
   await sendAndConfirmTransaction(connection, tx1, [payer, ...signers1], {
     skipPreflight: false,
     commitment: 'max',
     preflightCommitment: 'recent',
   })
 
-  console.log('*** sending TX 2');
+  console.log('*** sending TX 2')
   await sendAndConfirmTransaction(connection, tx2, [payer, ...signers2], {
     skipPreflight: false,
     commitment: 'max',
     preflightCommitment: 'recent',
   })
 
-  console.log('*** all transactions completed', market.publicKey.toString());
-
+  console.log('*** all transactions completed', market.publicKey.toString())
 })()

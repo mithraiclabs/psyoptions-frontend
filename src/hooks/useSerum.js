c64ce4d0e1ccda913692da584965bf4d3cb62cc3
Tommy Johnson <tommyjohnson1028@gmail.com>
2021-05-24 20:53:31 -0700

    fix error log

--- src/hooks/useSerum.js
+++ src/hooks/useSerum.js
@@ -38,7 +38,7 @@ const useSerum = () => {
           dexProgramId,
         )
       } catch (err) {
-        console.error(err, mintA, mintB)
+        console.error(err)
         error = err.message
         pushNotification({
           severity: 'error',

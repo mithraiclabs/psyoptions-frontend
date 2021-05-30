62557d640ba6afa805ed699466d83db50b5aed6c
Taylor123 <cjohns28@terpmail.umd.edu>
2021-05-29 21:35:22 -0700

    fix websocket and move gql url to env file

--- src/components/App.js
+++ src/components/App.js
@@ -1,39 +1,16 @@
-import { SubscriptionClient } from 'graphql-subscriptions-client'
 import React, { useEffect } from 'react'
-import {
-  createClient,
-  defaultExchanges,
-  Provider,
-  subscriptionExchange,
-} from 'urql'
 import Store from '../context/store'
 import useOptionsMarkets from '../hooks/useOptionsMarkets'
+import { GraphQLProvider } from './GraphQLProvider'
 import Router from './Router'
-
-const GRAPHQL_URL = 'http://hasura:8080/v1/graphql'
-const subscriptionClient = new SubscriptionClient(
-  GRAPHQL_URL.replace('http', 'ws'),
-  { reconnect: true },
-)
-const client = createClient({
-  url: GRAPHQL_URL,
-  exchanges: [
-    ...defaultExchanges,
-    subscriptionExchange({
-      forwardSubscription(operation) {
-        return subscriptionClient.request(operation)
-      },
-    }),
-  ],
-})
 
 const WrappedApp = (props) => {
   return (
-    <Provider value={client}>
+    <GraphQLProvider>
       <Store>
         <App {...props} />
       </Store>
-    </Provider>
+    </GraphQLProvider>
   )
 }
 

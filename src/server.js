330bd2ae43049736acec1a9988cb72d32a857b16
Tommy Johnson <tommyjohnson1028@gmail.com>
2021-06-20 21:30:10 -0700

    update container security group

--- src/server.js
+++ src/server.js
@@ -143,8 +143,9 @@ server.use((req, res) => {
 })
 
 const port = process.env.PORT || 3000
-console.log(`\nServer listening on port: ${port}\n`)
+
 server.listen(port, () => {
+  console.log(`\nServer listening on port: ${port}\n`)
   if (process.env.NODE_ENV === 'development') {
     console.log(`\nServer listening at http://localhost:${port}/\n`)
   }

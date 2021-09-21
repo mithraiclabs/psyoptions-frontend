bcafba10a24a25a48ae166194f7e4f6799ceac77
Tommy Johnson <tommyjohnson1028@gmail.com>
2021-09-20 18:39:58 -0700

    add new exerciseOption binding

--- src/hooks/useExerciseOpenPosition.tsx
+++ src/hooks/useExerciseOpenPosition.tsx
@@ -2,6 +2,11 @@ import { useCallback } from 'react';
 import { PublicKey, Transaction } from '@solana/web3.js';
 import { exerciseCoveredCall } from '@mithraic-labs/psyoptions';
 import BN from 'bn.js';
+import {
+  PSY_AMERICAN_PROGRAM_IDS,
+  ProgramVersions,
+  instructions,
+} from '@mithraic-labs/psy-american';
 
 import useConnection from './useConnection';
 import useWallet from './useWallet';
@@ -10,6 +15,8 @@ import useSendTransaction from './useSendTransaction';
 import { OptionMarket } from '../types';
 import { createAssociatedTokenAccountInstruction } from '../utils/instructions';
 import useOwnedTokenAccounts from './useOwnedTokenAccounts';
+import { useAmericanPsyOptionsProgram } from './useAmericanPsyOptionsProgram';
+import { uiOptionMarketToProtocolOptionMarket } from '../utils/typeConversions';
 
 // TODO add support for wrapped solana as quote asset???
 const useExerciseOpenPosition = (
@@ -18,72 +25,94 @@ const useExerciseOpenPosition = (
   exerciserUnderlyingAssetKey: PublicKey | null,
   exerciserContractTokenKey: PublicKey | null,
 ): ((size: number) => Promise<void>) => {
+  const program = useAmericanPsyOptionsProgram();
   const { subscribeToTokenAccount } = useOwnedTokenAccounts();
   const { pushErrorNotification } = useNotifications();
   const { connection } = useConnection();
   const { sendTransaction } = useSendTransaction();
   const { wallet, pubKey } = useWallet();
 
-  return useCallback(async (size) => {
-    try {
-      const transaction = new Transaction();
+  return useCallback(
+    async (size) => {
+      try {
+        const transaction = new Transaction();
 
-      let _exerciserUnderlyingAssetKey = exerciserUnderlyingAssetKey;
-      if (!_exerciserUnderlyingAssetKey) {
-        // Create a SPL Token account for this base account if the wallet doesn't have one
-        const [createOptAccountTx, newTokenAccountKey] =
-          await createAssociatedTokenAccountInstruction({
-            payer: pubKey,
-            owner: pubKey,
-            mintPublicKey: market.underlyingAssetMintKey,
-          });
-        transaction.add(createOptAccountTx);
-        subscribeToTokenAccount(newTokenAccountKey);
-        _exerciserUnderlyingAssetKey = newTokenAccountKey;
+        let _exerciserUnderlyingAssetKey = exerciserUnderlyingAssetKey;
+        if (!_exerciserUnderlyingAssetKey) {
+          // Create a SPL Token account for this base account if the wallet doesn't have one
+          const [createOptAccountTx, newTokenAccountKey] =
+            await createAssociatedTokenAccountInstruction({
+              payer: pubKey,
+              owner: pubKey,
+              mintPublicKey: market.underlyingAssetMintKey,
+            });
+          transaction.add(createOptAccountTx);
+          subscribeToTokenAccount(newTokenAccountKey);
+          _exerciserUnderlyingAssetKey = newTokenAccountKey;
+        }
+
+        let exerciseTx: Transaction;
+        if (
+          PSY_AMERICAN_PROGRAM_IDS[market.psyOptionsProgramId.toString()] ===
+          ProgramVersions.V1
+        ) {
+          ({ transaction: exerciseTx } = await exerciseCoveredCall({
+            connection,
+            payerKey: pubKey,
+            programId: market.psyOptionsProgramId,
+            optionMintKey: market.optionMintKey,
+            optionMarketKey: market.optionMarketKey,
+            exerciserQuoteAssetKey,
+            exerciserUnderlyingAssetKey: _exerciserUnderlyingAssetKey,
+            exerciserQuoteAssetAuthorityKey: pubKey,
+            underlyingAssetPoolKey: market.underlyingAssetPoolKey,
+            quoteAssetPoolKey: market.quoteAssetPoolKey,
+            optionTokenKey: exerciserContractTokenKey,
+            optionTokenAuthorityKey: pubKey,
+            quoteAssetMintKey: market.quoteAssetMintKey,
+            size: new BN(size),
+          }));
+        } else {
+          const ix = await instructions.exerciseOptionsInstruction(
+            program,
+            new BN(size),
+            uiOptionMarketToProtocolOptionMarket(market),
+            exerciserContractTokenKey,
+            _exerciserUnderlyingAssetKey,
+            exerciserQuoteAssetKey,
+          );
+          exerciseTx = new Transaction().add(ix);
+        }
+
+        transaction.add(exerciseTx);
+
+        // TODO add the Asset Pair to the push note messages
+        await sendTransaction({
+          transaction,
+          wallet,
+          connection,
+          sendingMessage: 'Processing: Exercise Option',
+          successMessage: 'Confirmed: Exercise Option',
+        });
+      } catch (err) {
+        pushErrorNotification(err);
       }
-
-      const { transaction: tx } = await exerciseCoveredCall({
-        connection,
-        payerKey: pubKey,
-        programId: market.psyOptionsProgramId,
-        optionMintKey: market.optionMintKey,
-        optionMarketKey: market.optionMarketKey,
-        exerciserQuoteAssetKey,
-        exerciserUnderlyingAssetKey: _exerciserUnderlyingAssetKey,
-        exerciserQuoteAssetAuthorityKey: pubKey,
-        underlyingAssetPoolKey: market.underlyingAssetPoolKey,
-        quoteAssetPoolKey: market.quoteAssetPoolKey,
-        optionTokenKey: exerciserContractTokenKey,
-        optionTokenAuthorityKey: pubKey,
-        quoteAssetMintKey: market.quoteAssetMintKey,
-        size: new BN(size),
-      });
-      transaction.add(tx);
-
-      // TODO add the Asset Pair to the push note messages
-      await sendTransaction({
-        transaction,
-        wallet,
-        connection,
-        sendingMessage: 'Processing: Exercise Option',
-        successMessage: 'Confirmed: Exercise Option',
-      });
-    } catch (err) {
-      pushErrorNotification(err);
-    }
-    return null;
-  }, [
-    connection,
-    pubKey,
-    market,
-    exerciserQuoteAssetKey,
-    exerciserUnderlyingAssetKey,
-    exerciserContractTokenKey,
-    wallet,
-    pushErrorNotification,
-    sendTransaction,
-    subscribeToTokenAccount,
-  ]);
+      return null;
+    },
+    [
+      connection,
+      pubKey,
+      market,
+      exerciserQuoteAssetKey,
+      exerciserUnderlyingAssetKey,
+      exerciserContractTokenKey,
+      program,
+      wallet,
+      pushErrorNotification,
+      sendTransaction,
+      subscribeToTokenAccount,
+    ],
+  );
 };
 
 export default useExerciseOpenPosition;

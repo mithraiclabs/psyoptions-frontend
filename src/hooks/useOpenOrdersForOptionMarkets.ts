69fa6352eb7cf628ee92118534037c48227bba56
Jason Chitla <jasonchitla@hotmail.com>
2021-10-05 14:02:23 -0400

    fix useOpenOrdersForOptionMarkets
    sicko mode

--- src/hooks/useOpenOrdersForOptionMarkets.ts
+++ src/hooks/useOpenOrdersForOptionMarkets.ts
@@ -3,21 +3,25 @@ import { OpenOrders } from '@mithraic-labs/serum';
 import { useState, useEffect } from 'react';
 import { useAmericanPsyOptionsProgram } from './useAmericanPsyOptionsProgram';
 import useConnection from './useConnection';
+import useWallet from './useWallet';
 
 /**
  * Get open orders for a user for option market keys
  *
- * @returns OpenOrders[] | null
+ * @returns OpenOrders[]
  */
 export const useOpenOrdersForOptionMarkets = () => {
-  const [openOrders, setOpenOrders] = useState([] as OpenOrders[] | null);
+  const [openOrders, setOpenOrders] = useState([] as OpenOrders[]);
   const program = useAmericanPsyOptionsProgram();
   const { dexProgramId } = useConnection();
+  const { pubKey } = useWallet();
 
   useEffect(() => {
     (async () => {
+      if (!program || !dexProgramId || !pubKey) return;
       const optionMarketWithKeys = await getAllOptionAccounts(program);
       const keys = optionMarketWithKeys.map(marketInfo => marketInfo.key);
+
       const orders = await serumUtils.findOpenOrdersForOptionMarkets(
         program,
         dexProgramId,
@@ -27,7 +31,7 @@ export const useOpenOrdersForOptionMarkets = () => {
       // #TODO: remove as any
       setOpenOrders(orders as any);
     })();
-  }, [program, dexProgramId]);
+  }, [program, dexProgramId, pubKey]);
 
   return { openOrders };
 };

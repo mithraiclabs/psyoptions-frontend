1b8ff32a30f6b73a5fcd2ad8f8b9829a335552f6
Taylor Johnson <nftaylor11@gmail.com>
2021-10-17 20:35:00 -0500

    Fix/mint wsol (#640)
    * handle wrapped solana when minting options
    
    * clean up and make 1 tx

--- src/hooks/PsyOptionsAPI/useMintOptions.ts
+++ src/hooks/PsyOptionsAPI/useMintOptions.ts
@@ -1,13 +1,22 @@
-import { instructions, OptionMarketWithKey } from '@mithraic-labs/psy-american';
+import {
+  feeAmountPerContract,
+  instructions,
+  OptionMarketWithKey,
+} from '@mithraic-labs/psy-american';
 import { BN } from '@project-serum/anchor';
 import {
   ASSOCIATED_TOKEN_PROGRAM_ID,
   Token,
   TOKEN_PROGRAM_ID,
 } from '@solana/spl-token';
-import { Signer, Transaction } from '@solana/web3.js';
+import { PublicKey, Signer, Transaction } from '@solana/web3.js';
 import { useCallback } from 'react';
+import { useSolanaMeta } from '../../context/SolanaMetaContext';
 import { NotificationSeverity } from '../../types';
+import {
+  initializeTokenAccountTx,
+  WRAPPED_SOL_ADDRESS,
+} from '../../utils/token';
 import { useAmericanPsyOptionsProgram } from '../useAmericanPsyOptionsProgram';
 import useNotifications from '../useNotifications';
 import useWallet from '../useWallet';
@@ -18,101 +27,159 @@ export const useMintOptions = (): ((
 ) => Promise<void>) => {
   const program = useAmericanPsyOptionsProgram();
   const { pushErrorNotification, pushNotification } = useNotifications();
+  const { splTokenAccountRentBalance } = useSolanaMeta();
   const { wallet } = useWallet();
 
   return useCallback(
     async (option, size) => {
-      if (!wallet || !program) {
+      if (!wallet || !program || !splTokenAccountRentBalance) {
         return;
       }
-      const [optionTokenDest, writerTokenDest, underlyingAssetSource] =
-        await Promise.all([
-          Token.getAssociatedTokenAddress(
-            ASSOCIATED_TOKEN_PROGRAM_ID,
-            TOKEN_PROGRAM_ID,
-            option.optionMint,
-            wallet.publicKey,
-          ),
-          Token.getAssociatedTokenAddress(
-            ASSOCIATED_TOKEN_PROGRAM_ID,
-            TOKEN_PROGRAM_ID,
-            option.writerTokenMint,
-            wallet.publicKey,
-          ),
-          Token.getAssociatedTokenAddress(
-            ASSOCIATED_TOKEN_PROGRAM_ID,
-            TOKEN_PROGRAM_ID,
-            option.underlyingAssetMint,
-            wallet.publicKey,
-          ),
-        ]);
-      const optionToken = new Token(
-        program.provider.connection,
-        option.optionMint,
-        TOKEN_PROGRAM_ID,
-        null as unknown as Signer,
-      );
-      const writerToken = new Token(
-        program.provider.connection,
-        option.writerTokenMint,
-        TOKEN_PROGRAM_ID,
-        null as unknown as Signer,
-      );
-      const transaction = new Transaction();
-      try {
-        await optionToken.getAccountInfo(optionTokenDest);
-      } catch (err) {
-        // no account found, generate instruction to create it
-        const ix = Token.createAssociatedTokenAccountInstruction(
-          ASSOCIATED_TOKEN_PROGRAM_ID,
-          TOKEN_PROGRAM_ID,
-          option.optionMint,
-          optionTokenDest,
-          wallet.publicKey,
-          wallet.publicKey,
-        );
-        transaction.add(ix);
-      }
-      try {
-        await writerToken.getAccountInfo(option.writerTokenMint);
-      } catch (err) {
-        // no account found, generate instruction to create it
-        const ix = Token.createAssociatedTokenAccountInstruction(
-          ASSOCIATED_TOKEN_PROGRAM_ID,
-          TOKEN_PROGRAM_ID,
-          option.writerTokenMint,
-          writerTokenDest,
-          wallet.publicKey,
-          wallet.publicKey,
-        );
-        transaction.add(ix);
-      }
-      if (transaction.instructions.length) {
-        // create associated token accounts if there are any instructions
-        await program.provider.send(transaction);
-      }
-
       try {
         pushNotification({
           severity: NotificationSeverity.INFO,
           message: 'Processing: Mint Options',
         });
-        await instructions.mintOptionsTx(
-          program,
-          optionTokenDest,
-          writerTokenDest,
-          underlyingAssetSource,
-          new BN(size),
-          option,
+
+        const transaction = new Transaction();
+        const signers: Signer[] = [];
+        let _underlyingAssetSourceKey: PublicKey | null = null;
+        if (option.underlyingAssetMint.toString() === WRAPPED_SOL_ADDRESS) {
+          const fees = feeAmountPerContract(option.underlyingAmountPerContract);
+          const lamports = option.underlyingAmountPerContract
+            .add(fees)
+            .mul(new BN(size));
+          const { transaction: wrapSolTx, newTokenAccount: wrappedSolAccount } =
+            await initializeTokenAccountTx({
+              connection: program.provider.connection,
+              payerKey: wallet.publicKey,
+              mintPublicKey: new PublicKey(WRAPPED_SOL_ADDRESS),
+              owner: wallet.publicKey,
+              rentBalance: splTokenAccountRentBalance,
+              extraLamports: lamports.toNumber(),
+            });
+          transaction.add(wrapSolTx);
+          signers.push(wrappedSolAccount);
+          _underlyingAssetSourceKey = wrappedSolAccount.publicKey;
+        }
+
+        const [optionTokenDest, writerTokenDest, underlyingAssetSource] =
+          await Promise.all([
+            Token.getAssociatedTokenAddress(
+              ASSOCIATED_TOKEN_PROGRAM_ID,
+              TOKEN_PROGRAM_ID,
+              option.optionMint,
+              wallet.publicKey,
+            ),
+            Token.getAssociatedTokenAddress(
+              ASSOCIATED_TOKEN_PROGRAM_ID,
+              TOKEN_PROGRAM_ID,
+              option.writerTokenMint,
+              wallet.publicKey,
+            ),
+            ...(option.underlyingAssetMint.toString() === WRAPPED_SOL_ADDRESS
+              ? []
+              : [
+                  Token.getAssociatedTokenAddress(
+                    ASSOCIATED_TOKEN_PROGRAM_ID,
+                    TOKEN_PROGRAM_ID,
+                    option.underlyingAssetMint,
+                    wallet.publicKey,
+                  ),
+                ]),
+          ]);
+        if (underlyingAssetSource) {
+          _underlyingAssetSourceKey = underlyingAssetSource;
+        }
+        const optionToken = new Token(
+          program.provider.connection,
+          option.optionMint,
+          TOKEN_PROGRAM_ID,
+          null as unknown as Signer,
         );
+        const writerToken = new Token(
+          program.provider.connection,
+          option.writerTokenMint,
+          TOKEN_PROGRAM_ID,
+          null as unknown as Signer,
+        );
+        try {
+          await optionToken.getAccountInfo(optionTokenDest);
+        } catch (err) {
+          // no account found, generate instruction to create it
+          const ix = Token.createAssociatedTokenAccountInstruction(
+            ASSOCIATED_TOKEN_PROGRAM_ID,
+            TOKEN_PROGRAM_ID,
+            option.optionMint,
+            optionTokenDest,
+            wallet.publicKey,
+            wallet.publicKey,
+          );
+          transaction.add(ix);
+        }
+        try {
+          await writerToken.getAccountInfo(writerTokenDest);
+        } catch (err) {
+          // no account found, generate instruction to create it
+          const ix = Token.createAssociatedTokenAccountInstruction(
+            ASSOCIATED_TOKEN_PROGRAM_ID,
+            TOKEN_PROGRAM_ID,
+            option.writerTokenMint,
+            writerTokenDest,
+            wallet.publicKey,
+            wallet.publicKey,
+          );
+          transaction.add(ix);
+        }
+
+        const { ix, signers: _signers } =
+          await instructions.mintOptionInstruction(
+            program,
+            optionTokenDest,
+            writerTokenDest,
+            _underlyingAssetSourceKey as PublicKey,
+            new BN(size),
+            option,
+          );
+        transaction.add(ix);
+        signers.concat(_signers);
+        if (option.underlyingAssetMint.toString() === WRAPPED_SOL_ADDRESS) {
+          const closeWSolIx = Token.createCloseAccountInstruction(
+            TOKEN_PROGRAM_ID,
+            _underlyingAssetSourceKey as PublicKey,
+            wallet.publicKey, // Send any remaining SOL to the owner
+            wallet.publicKey,
+            [],
+          );
+          transaction.add(closeWSolIx);
+        }
+        if (signers.length) {
+          transaction.feePayer = wallet.publicKey;
+          transaction.recentBlockhash = (
+            await program.provider.connection.getRecentBlockhash('max')
+          ).blockhash;
+          transaction.partialSign(...signers);
+        }
+        await wallet.signTransaction(transaction);
+
+        await program.provider.connection.sendRawTransaction(
+          transaction.serialize(),
+        );
+
         pushNotification({
           severity: NotificationSeverity.SUCCESS,
           message: 'Confirmed: Mint Options',
         });
       } catch (err) {
-        pushErrorNotification(err);
+        return pushErrorNotification(err);
       }
     },
-    [program, pushErrorNotification, pushNotification, wallet],
+    [
+      program,
+      pushErrorNotification,
+      pushNotification,
+      splTokenAccountRentBalance,
+      wallet,
+    ],
   );
 };

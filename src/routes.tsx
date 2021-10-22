8740174d038a96d9d3be585cf840976abb2e5f39
Jason Chitla <jasonchitla@hotmail.com>
2021-10-22 16:52:12 -0400

    add simple UI routes

--- src/routes.tsx
+++ src/routes.tsx
@@ -6,7 +6,12 @@ import Faucet from "./pages/faucets";
 import InitializeMarkets from "./pages/initialize-market";
 import Custom404 from "./404";
 import Portfolio from "./pages/portfolio";
-import SimpleUI from "./pages/simple/choose-asset";
+import SimpleUIChooseAsset from "./pages/simple/choose-asset";
+import SimpleUIUpOrDown from "./pages/simple/up-or-down";
+import SimpleUIChooseExpiration from "./pages/simple/choose-expiration";
+import SimpleUIChooseStrike from "./pages/simple/choose-strike";
+import SimpleUIOrderSettings from "./pages/simple/order-settings";
+import SimpleUIConfirmOrder from "./pages/simple/confirm-order";
 import Mints from "./pages/mint";
 
 export function Routes() {
@@ -19,7 +24,12 @@ export function Routes() {
       <Route exact path="/initialize-market" children={<InitializeMarkets />} />
       <Route exact path="/faucets" children={<Faucet />} />
       <Route exact path="/portfolio" children={<Portfolio />} />
-      <Route exact path="/simple/choose-asset" children={<SimpleUI />} />
+      <Route exact path="/simple/choose-asset" children={<SimpleUIChooseAsset />} />
+      <Route exact path="/simple/up-or-down" children={<SimpleUIUpOrDown />} />
+      <Route exact path="/simple/choose-expiration" children={<SimpleUIChooseExpiration />} />
+      <Route exact path="/simple/choose-strike" children={<SimpleUIChooseStrike />} />
+      <Route exact path="/simple/order-settings" children={<SimpleUIOrderSettings />} />
+      <Route exact path="/simple/confirm-order" children={<SimpleUIConfirmOrder />} />
       <Route exact path="/not-found" children={<Custom404 />} />
       <Route exact path="*"><Redirect
         to={{

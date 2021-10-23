c124fdbcc6b1fb74a8c031fc3c4bf6868da265f1
Taylor123 <cjohns28@terpmail.umd.edu>
2021-10-23 18:17:36 -0500

    wire up contract size to recoil

--- src/components/ContractSizeSelector.tsx
+++ src/components/ContractSizeSelector.tsx
@@ -1,24 +1,37 @@
-import React from 'react';
+import { BN } from '@project-serum/anchor';
+import React, { useCallback, useMemo } from 'react';
 import Select from './Select';
 
 /**
  * Component for selecting supported contract sizes
  */
 export const ContractSizeSelector: React.VFC<{
-  options: {
-    value: number;
-    text: string;
-  }[];
-  onChange: (
-    e: React.ChangeEvent<{
-      name?: string;
-      value: string;
-    }>,
-  ) => void;
-  value: number | string;
-}> = ({ options, onChange, value }) => {
+  decimals: number;
+  options: BN[];
+  onChange: (size: BN) => void;
+  value: BN;
+}> = ({ decimals, options, onChange, value }) => {
+  const _options = useMemo(
+    () =>
+      options.map((s) => {
+        const size = s.toNumber() * 10 ** -decimals;
+        return {
+          text: size.toString(),
+          value: s,
+        };
+      }),
+    [decimals, options],
+  );
+  const _onChange = useCallback(
+    (e) => {
+      onChange(e.value);
+    },
+    [onChange],
+  );
+
   return (
     <Select
+      disabled={!_options.length}
       formControlOptions={{
         variant: 'filled',
         style: {
@@ -27,8 +40,8 @@ export const ContractSizeSelector: React.VFC<{
       }}
       label="Contract Size"
       value={value}
-      onChange={onChange}
-      options={options}
+      onChange={_onChange}
+      options={_options}
     />
   );
 };

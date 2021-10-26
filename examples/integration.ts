9d84de5abb61b3fad36e19b95d6a68c469ef6b5a
Tommy Johnson <tommyjohnson1028@gmail.com>
2021-10-25 20:31:42 -0700

    WIP setting up for wrapped SOL examples

--- examples/integration.ts
+++ examples/integration.ts
@@ -2,6 +2,7 @@ import { PsyAmericanIdl } from '@mithraic-labs/psy-american';
 import { Program, Provider } from '@project-serum/anchor';
 import { NodeWallet } from '@project-serum/anchor/dist/cjs/provider';
 import { Commitment, Connection, PublicKey } from '@solana/web3.js';
+import { MarketMeta } from '@mithraic-labs/market-meta';
 import * as fs from 'fs';
 import * as os from 'os';
 
@@ -11,6 +12,10 @@ function readKeypair() {
       fs.readFileSync(os.homedir() + '/.config/solana/devnet.json', 'utf-8'),
   );
 }
+
+const WRAPPED_SOL_ADDRESS = new PublicKey(
+  'So11111111111111111111111111111111111111112',
+);
 
 // Below we create the Anchor Program from the PsyAmerican IDL,
 //  devnet connection, and wallet keypair
@@ -24,3 +29,13 @@ const connection = new Connection(
 const wallet = new NodeWallet(readKeypair());
 const provider = new Provider(connection, wallet, { commitment: 'processed' });
 const program = new Program(PsyAmericanIdl, PSY_PROGRAM_ID, provider);
+
+// Filter devnet markets to only unexpired markets
+// TODO: Filter for only SOL markets
+const activeDevnetMarkets = MarketMeta.devnet.optionMarkets.filter(
+  (marketMeta) => marketMeta.expiration * 1000 > new Date().getTime(),
+);
+
+// TODO: Provide gist for minting an option
+
+// TODO: Provide example for posting an order to the permissioned markets

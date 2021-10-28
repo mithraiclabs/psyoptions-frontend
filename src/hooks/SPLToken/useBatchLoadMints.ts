5f91bce4afa88a41a2d8b048998a1c7a90d98efa
Jason Chitla <jasonchitla@hotmail.com>
2021-10-28 13:05:02 -0400

    change to map, use await

--- src/hooks/SPLToken/useBatchLoadMints.ts
+++ src/hooks/SPLToken/useBatchLoadMints.ts
@@ -21,41 +21,36 @@ export const useBatchLoadMints = (mints: PublicKey[]) => {
     (async () => {
       try {
         const groupOfMints: PublicKey[][] = chunkArray(mints, 100);
-        const getMultipleAccountsInfoPromises: Promise<AccountInfo<Buffer>[]>[] = [];
-        groupOfMints.forEach(mints => {
-          getMultipleAccountsInfoPromises.push(connection.getMultipleAccountsInfo(mints));
+        const getMultipleAccountsInfoPromises: Promise<AccountInfo<Buffer>[]>[] = groupOfMints.map(mints => {
+          return connection.getMultipleAccountsInfo(mints);
         });
-        Promise.all(getMultipleAccountsInfoPromises).then(results => {
-          const infos: AccountInfo<Buffer>[] = results.flat();
-          const mintInfos: Record<string, MintInfo> = {};
-          infos.forEach((info, index) => {
-            if (!info)
-              return;
-            const mintInfo = MintLayout.decode(info.data);
-            if (mintInfo.mintAuthorityOption === 0) {
-              mintInfo.mintAuthority = null;
-            } else {
-              mintInfo.mintAuthority = new PublicKey(mintInfo.mintAuthority);
-            }
-  
-            mintInfo.supply = u64.fromBuffer(mintInfo.supply);
-            mintInfo.isInitialized = mintInfo.isInitialized !== 0;
-  
-            if (mintInfo.freezeAuthorityOption === 0) {
-              mintInfo.freezeAuthority = null;
-            } else {
-              mintInfo.freezeAuthority = new PublicKey(mintInfo.freezeAuthority);
-            }
-            mintInfos[mints[index].toString()] = mintInfo;
-          });
-          setSPLTokenMints((_mintInfos) => ({
-            ..._mintInfos,
-            ...mintInfos,
-          }));
-        }).catch(err => {
-          console.error(err);
-          Sentry.captureException(err);
+        const results = await Promise.all(getMultipleAccountsInfoPromises);
+        const infos: AccountInfo<Buffer>[] = results.flat();
+        const mintInfos: Record<string, MintInfo> = {};
+        infos.forEach((info, index) => {
+          if (!info)
+            return;
+          const mintInfo = MintLayout.decode(info.data);
+          if (mintInfo.mintAuthorityOption === 0) {
+            mintInfo.mintAuthority = null;
+          } else {
+            mintInfo.mintAuthority = new PublicKey(mintInfo.mintAuthority);
+          }
+
+          mintInfo.supply = u64.fromBuffer(mintInfo.supply);
+          mintInfo.isInitialized = mintInfo.isInitialized !== 0;
+
+          if (mintInfo.freezeAuthorityOption === 0) {
+            mintInfo.freezeAuthority = null;
+          } else {
+            mintInfo.freezeAuthority = new PublicKey(mintInfo.freezeAuthority);
+          }
+          mintInfos[mints[index].toString()] = mintInfo;
         });
+        setSPLTokenMints((_mintInfos) => ({
+          ..._mintInfos,
+          ...mintInfos,
+        }));
       } catch (err) {
         console.error(err);
         Sentry.captureException(err);

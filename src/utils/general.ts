c999197f785dbfebb695b582ba86cac7fb4cea7a
Jason Chitla <jasonchitla@hotmail.com>
2021-10-28 13:06:20 -0400

    useBatchLoadMints now supports larger than 100 mint array (#679)
    * useBatchLoadMints now supports larger than 100 mint array
    
    * change to map, use await

--- src/utils/general.ts
+++ src/utils/general.ts
@@ -1,1 +1,20 @@
 export const isTrue = (val: string | boolean) => val === true || val === 'true';
+
+/**
+ * Returns an array with arrays of the given size.
+ *
+ * @param myArray {Array} Array to split
+ * @param chunkSize {Integer} Size of every group
+ */
+export const chunkArray = (myArray, chunkSize) => {
+  var index = 0;
+  var arrayLength = myArray.length;
+  var tempArray = [];
+  
+  for (index = 0; index < arrayLength; index += chunkSize) {
+    const myChunk = myArray.slice(index, index + chunkSize);
+    tempArray.push(myChunk);
+  }
+
+  return tempArray;
+};

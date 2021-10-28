306ae1326f1d1e04eec8918b9121f99d41ae884e
Jason Chitla <jasonchitla@hotmail.com>
2021-10-28 12:45:53 -0400

    useBatchLoadMints now supports larger than 100 mint array

--- src/hooks/SPLToken/useBatchLoadMints.ts
+++ src/hooks/SPLToken/useBatchLoadMints.ts
@@ -1,9 +1,10 @@
 import { MintInfo, MintLayout, u64 } from '@solana/spl-token';
-import { PublicKey } from '@solana/web3.js';
+import { AccountInfo, PublicKey } from '@solana/web3.js';
 import { useEffect } from 'react';
 import * as Sentry from '@sentry/react';
 import { useSPLTokenMints } from '../../context/SPLTokenMintsContext';
 import useConnection from '../useConnection';
+import { chunkArray } from '@/utils/general';
 
 /**
  * Fetch and update global context state with SPL Token mint info.
@@ -19,30 +20,42 @@ export const useBatchLoadMints = (mints: PublicKey[]) => {
     if (!mints.length) return;
     (async () => {
       try {
-        const infos = await connection.getMultipleAccountsInfo(mints);
-        const mintInfos: Record<string, MintInfo> = {};
-        infos.forEach((info, index) => {
-          const mintInfo = MintLayout.decode(info.data);
-          if (mintInfo.mintAuthorityOption === 0) {
-            mintInfo.mintAuthority = null;
-          } else {
-            mintInfo.mintAuthority = new PublicKey(mintInfo.mintAuthority);
-          }
-
-          mintInfo.supply = u64.fromBuffer(mintInfo.supply);
-          mintInfo.isInitialized = mintInfo.isInitialized !== 0;
-
-          if (mintInfo.freezeAuthorityOption === 0) {
-            mintInfo.freezeAuthority = null;
-          } else {
-            mintInfo.freezeAuthority = new PublicKey(mintInfo.freezeAuthority);
-          }
-          mintInfos[mints[index].toString()] = mintInfo;
+        const groupOfMints: PublicKey[][] = chunkArray(mints, 100);
+        const getMultipleAccountsInfoPromises: Promise<AccountInfo<Buffer>[]>[] = [];
+        groupOfMints.forEach(mints => {
+          getMultipleAccountsInfoPromises.push(connection.getMultipleAccountsInfo(mints));
         });
-        setSPLTokenMints((_mintInfos) => ({
-          ..._mintInfos,
-          ...mintInfos,
-        }));
+        Promise.all(getMultipleAccountsInfoPromises).then(results => {
+          const infos: AccountInfo<Buffer>[] = results.flat();
+          const mintInfos: Record<string, MintInfo> = {};
+          infos.forEach((info, index) => {
+            if (!info)
+              return;
+            const mintInfo = MintLayout.decode(info.data);
+            if (mintInfo.mintAuthorityOption === 0) {
+              mintInfo.mintAuthority = null;
+            } else {
+              mintInfo.mintAuthority = new PublicKey(mintInfo.mintAuthority);
+            }
+  
+            mintInfo.supply = u64.fromBuffer(mintInfo.supply);
+            mintInfo.isInitialized = mintInfo.isInitialized !== 0;
+  
+            if (mintInfo.freezeAuthorityOption === 0) {
+              mintInfo.freezeAuthority = null;
+            } else {
+              mintInfo.freezeAuthority = new PublicKey(mintInfo.freezeAuthority);
+            }
+            mintInfos[mints[index].toString()] = mintInfo;
+          });
+          setSPLTokenMints((_mintInfos) => ({
+            ..._mintInfos,
+            ...mintInfos,
+          }));
+        }).catch(err => {
+          console.error(err);
+          Sentry.captureException(err);
+        });
       } catch (err) {
         console.error(err);
         Sentry.captureException(err);

cf079dd67f2dacb53d2a35b56238baf64f909475
Taylor123 <cjohns28@terpmail.umd.edu>
2021-12-05 14:34:40 -0600

    optimize option state loading around wallet connect/disconnect

--- src/hooks/PsyOptionsAPI/useLoadOptionMarkets.ts
+++ src/hooks/PsyOptionsAPI/useLoadOptionMarkets.ts
@@ -1,8 +1,9 @@
 import { OptionMarket } from '@mithraic-labs/psy-american';
 import { ProgramAccount } from '@project-serum/anchor';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { useInsertOptions } from '../../recoil';
 import { useAmericanPsyOptionsProgram } from '../useAmericanPsyOptionsProgram';
+import usePrevious from '../usePrevious';
 
 /**
  * Load all options on chain and store in Recoil state
@@ -10,16 +11,31 @@ import { useAmericanPsyOptionsProgram } from '../useAmericanPsyOptionsProgram';
 export const useLoadOptionMarkets = (): void => {
   const program = useAmericanPsyOptionsProgram();
   const upsertOptions = useInsertOptions(true);
+  const prevConnection = usePrevious(program?.provider.connection);
+  const hasInitialized = useRef(false);
 
   useEffect(() => {
+    if (
+      hasInitialized.current &&
+      program?.provider.connection &&
+      program?.provider.connection === prevConnection
+    ) {
+      // Do not attempt to update options state when the effect runs,
+      // but the connection (aka network) has not changed. This happens
+      // when a user connects/disconnects their wallet
+      return;
+    }
+    hasInitialized.current = true;
     (async () => {
       const options = (await program?.account.optionMarket.all()) as
         | ProgramAccount<OptionMarket>[]
         | null;
+
       if (options) {
         // update the option state
         upsertOptions(options);
       }
     })();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [program, upsertOptions]);
 };

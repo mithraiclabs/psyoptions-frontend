4ecbb85ca854aa22035ac4502f247b0c1f244351
Taylor123 <cjohns28@terpmail.umd.edu>
2021-12-11 15:29:46 -0600

    fix wallet balances on markets page

--- src/components/Markets/MarketsBalances.tsx
+++ src/components/Markets/MarketsBalances.tsx
@@ -3,39 +3,62 @@ import Box from '@material-ui/core/Box';
 import Avatar from '@material-ui/core/Avatar';
 import { LAMPORTS_PER_SOL } from '@solana/web3.js';
 import { useConnectedWallet } from '@saberhq/use-solana';
+import { useRecoilValue } from 'recoil';
+import BN from 'bn.js';
 import useAssetList from '../../hooks/useAssetList';
 import useOwnedTokenAccounts from '../../hooks/useOwnedTokenAccounts';
 import { getHighestAccount, WRAPPED_SOL_ADDRESS } from '../../utils/token';
 import useWalletInfo from '../../hooks/useWalletInfo';
+import { quoteMint, underlyingMint } from '../../recoil';
+import { useTokenByMint } from '../../hooks/useNetworkTokens';
+import { useNormalizeAmountOfMintBN } from '../../hooks/useNormalizeAmountOfMintBN';
 
 const Balances: React.FC = () => {
+  const _underlyingMint = useRecoilValue(underlyingMint);
+  const _quoteMint = useRecoilValue(quoteMint);
   const wallet = useConnectedWallet();
   const { balance } = useWalletInfo();
-  const { uAsset, qAsset, assetListLoading } = useAssetList();
+  const { assetListLoading } = useAssetList();
   const { ownedTokenAccounts, loadingOwnedTokenAccounts } =
     useOwnedTokenAccounts();
+  const underlyingAsset = useTokenByMint(_underlyingMint?.toString() ?? '');
+  const underlyingAssetSymbol =
+    underlyingAsset?.symbol ?? _underlyingMint?.toString() ?? '';
+  const normalizeUnderlyingBN = useNormalizeAmountOfMintBN(_underlyingMint);
+  const normalizeQuoteBN = useNormalizeAmountOfMintBN(_quoteMint);
+  const quoteAsset = useTokenByMint(_quoteMint?.toString() ?? '');
+  const quoteAssetSymbol = quoteAsset?.symbol ?? _quoteMint?.toString() ?? '';
   const [qAssetBalance, setQAssetBalance] = useState(0);
   const [uAssetBalance, setUAssetBalance] = useState(0);
 
   useEffect(() => {
-    const uAssetAccounts = ownedTokenAccounts[uAsset?.mintAddress ?? ''] || [];
-    const qAssetAccounts = ownedTokenAccounts[qAsset?.mintAddress ?? ''] || [];
+    const uAssetAccounts =
+      ownedTokenAccounts[_underlyingMint?.toString() ?? ''] || [];
+    const qAssetAccounts =
+      ownedTokenAccounts[_quoteMint?.toString() ?? ''] || [];
     const qAssetHighestAccount = getHighestAccount(qAssetAccounts);
-    const newQAssetBalance = qAssetHighestAccount
-      ? qAssetHighestAccount.amount / 10 ** (qAsset?.decimals ?? 0)
-      : 0;
-    setQAssetBalance(newQAssetBalance);
+    const newQAssetBalance = normalizeQuoteBN(
+      new BN(qAssetHighestAccount?.amount ?? 0),
+    );
+    setQAssetBalance(newQAssetBalance.toNumber());
 
     const uAssetHighestAccount = getHighestAccount(uAssetAccounts);
-    let newUAssetBalance = uAssetHighestAccount
-      ? uAssetHighestAccount.amount / 10 ** (uAsset?.decimals ?? 0)
-      : 0;
-    if (uAsset?.mintAddress === WRAPPED_SOL_ADDRESS) {
+    let newUAssetBalance = normalizeUnderlyingBN(
+      new BN(uAssetHighestAccount?.amount ?? 0),
+    ).toNumber();
+    if (_underlyingMint?.toString() === WRAPPED_SOL_ADDRESS) {
       // if asset is wrapped Sol, use balance of wallet account
       newUAssetBalance = balance ? balance / LAMPORTS_PER_SOL : 0;
     }
     setUAssetBalance(newUAssetBalance);
-  }, [ownedTokenAccounts, qAsset, uAsset, balance]);
+  }, [
+    ownedTokenAccounts,
+    balance,
+    _underlyingMint,
+    _quoteMint,
+    normalizeQuoteBN,
+    normalizeUnderlyingBN,
+  ]);
 
   if (!loadingOwnedTokenAccounts && !assetListLoading && wallet?.connected) {
     return (
@@ -51,16 +74,16 @@ const Balances: React.FC = () => {
           <Box display="flex" flexDirection={'row'} alignItems="center" mr={1}>
             <Avatar
               style={{ width: '20px', height: '20px', marginRight: '6px' }}
-              src={uAsset?.icon}
+              src={underlyingAsset?.logoURI}
             />{' '}
-            {uAssetBalance.toFixed(2)} {uAsset?.tokenSymbol}
+            {uAssetBalance.toFixed(2)} {underlyingAssetSymbol}
           </Box>
           <Box display="flex" flexDirection={'row'} alignItems="center" mx={1}>
             <Avatar
               style={{ width: '20px', height: '20px', marginRight: '6px' }}
-              src={qAsset?.icon}
+              src={quoteAsset?.logoURI}
             />{' '}
-            {qAssetBalance.toFixed(2)} {qAsset?.tokenSymbol}
+            {qAssetBalance.toFixed(2)} {quoteAssetSymbol}
           </Box>
         </Box>
       </Box>
